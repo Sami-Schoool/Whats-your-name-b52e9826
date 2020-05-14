50e58eca120293d919d24016082a346d8eb23d1f
Sami School <2077560@talnet.nl>
2020-05-14 16:14:26 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+    
+    //voor en achternaam vragen
+
+        $voornaam = readline("Wat is je voornaam? ");
+        $achternaam = readline("Wat is je achternaam? ");
+        echo "Jouw naam is: " . $voornaam . " " . $achternaam;
+        
+
+    
+
+?>
